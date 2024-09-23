157c147eabe275ebbb700a2f56bcce2a77f9bd66
tawhidislam22 <tawhidislam144@gmail.com>
2024-09-23 23:21:13 +0600

    donate js section done

--- js/index.js
+++ js/index.js
@@ -19,3 +19,35 @@ addEventListener('click',function(){
         alert('You have not enough money');
     }
 })
+//feni donate part
+document.getElementById('feni-donate').
+addEventListener('click',function(){
+    let balanceNumber=getTextFieldById('balance');
+    let feniDonateNumber=getTextFieldById('feni-donate-balance');
+    let feniDonateAmountNumber=getInputFieldById('feni-donate-amount');
+    if(balanceNumber>feniDonateAmountNumber){
+        feniDonateNumber+=feniDonateAmountNumber;
+        document.getElementById('feni-donate-balance').innerText=feniDonateNumber;
+        balanceNumber-=feniDonateAmountNumber;
+        document.getElementById('balance').innerText=balanceNumber;
+    }
+    else{
+        alert('You have not enough money');
+    }
+})
+//quota donate part
+document.getElementById('quota-donate').
+addEventListener('click',function(){
+    let balanceNumber=getTextFieldById('balance');
+    let quotaDonateNumber=getTextFieldById('quota-donate-balance');
+    let quotaDonateAmountNumber=getInputFieldById('quota-donate-amount');
+    if(balanceNumber>quotaDonateAmountNumber){
+        quotaDonateNumber+=quotaDonateAmountNumber;
+        document.getElementById('quota-donate-balance').innerText=quotaDonateNumber;
+        balanceNumber-=quotaDonateAmountNumber;
+        document.getElementById('balance').innerText=balanceNumber;
+    }
+    else{
+        alert('You have not enough money');
+    }
+})
