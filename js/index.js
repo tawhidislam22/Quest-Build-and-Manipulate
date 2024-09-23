5e914c595836d949c854165d7754e3303630e045
tawhidislam22 <tawhidislam144@gmail.com>
2024-09-24 01:52:50 +0600

    history section done

--- js/index.js
+++ js/index.js
@@ -14,6 +14,16 @@ addEventListener('click',function(){
         document.getElementById('noakhali-donate-balance').innerText=noakhaliDonateNumber;
         balanceNumber-=noakhaliDonateAmountNumber;
         document.getElementById('balance').innerText=balanceNumber;
+
+        const div=document.createElement('div');
+        div.classList.add('border','p-4','rounded-lg');
+        let now=new Date();
+
+        div.innerHTML=`
+        <h4 class="text-lg font-semibold text-[#111111] mb-2">${noakhaliDonateAmountNumber} Taka is Donated for famine-2024 at noakhali, Bangladesh</h4>
+        <p>${now}</p>
+        `;
+        document.getElementById('history-section').appendChild(div);
     }
     else{
         alert('You have not enough money');
@@ -30,6 +40,16 @@ addEventListener('click',function(){
         document.getElementById('feni-donate-balance').innerText=feniDonateNumber;
         balanceNumber-=feniDonateAmountNumber;
         document.getElementById('balance').innerText=balanceNumber;
+
+        const div=document.createElement('div');
+        div.classList.add('border','p-4','rounded-lg');
+        let now=new Date();
+
+        div.innerHTML=`
+        <h4 class="text-lg font-semibold text-[#111111] mb-2">${feniDonateAmountNumber} Taka is Donated for Flood Relief in Feni,Bangladesh</h4>
+        <p>${now}</p>
+        `;
+        document.getElementById('history-section').appendChild(div);
     }
     else{
         alert('You have not enough money');
@@ -46,6 +66,16 @@ addEventListener('click',function(){
         document.getElementById('quota-donate-balance').innerText=quotaDonateNumber;
         balanceNumber-=quotaDonateAmountNumber;
         document.getElementById('balance').innerText=balanceNumber;
+
+        const div=document.createElement('div');
+        div.classList.add('border','p-4','rounded-lg');
+        let now=new Date();
+
+        div.innerHTML=`
+        <h4 class="text-lg font-semibold text-[#111111] mb-2">${quotaDonateAmountNumber} Taka is Donated for Aid for Injured in the Quota Movement, Bangladesh</h4>
+        <p>${now}</p>
+        `;
+        document.getElementById('history-section').appendChild(div);
     }
     else{
         alert('You have not enough money');
---
+++ js/utlities.js
@@ -0,0 +1,21 @@
+document.getElementById('donate-btn').
+addEventListener('click',function(){
+    document.getElementById('donate-section').classList.remove('hidden');
+    document.getElementById('history-section').classList.add('hidden');
+    document.getElementById('donate-btn').classList.add('bg-[#B4F461]','text-[#111111]');
+    document.getElementById('history-btn').classList.remove('bg-[#B4F461]','text-[#111111]');
+    document.getElementById('history-btn').classList.add('text-[#11111171]');
+
+})
+
+document.getElementById('history-btn').
+addEventListener('click',function(){
+    document.getElementById('donate-section').classList.add('hidden');
+    document.getElementById('history-section').classList.remove('hidden');
+    document.getElementById('donate-btn').classList.add('text-[#11111171]', 'border' ,'border-gray-300');
+    document.getElementById('donate-btn').classList.remove('bg-[#B4F461]','text-[#111111]');
+    document.getElementById('history-btn').classList.add('bg-[#B4F461]','text-[#111111]');
+
+
+})
+
